a8c3acb8d4f961b9fa3f78c5437ceba356d86987
Fred1511 <secretariat@forsim.net>
2021-07-07 16:52:57 +0200

    Add (after creating if not exists) the geopackage file to the project

--- src/core/database.py
+++ src/core/database.py
@@ -2,3 +2,59 @@
 
 __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
+
+import os.path
+
+from PyQt5.QtWidgets import QMessageBox
+from qgis._core import QgsWkbTypes, QgsFields, QgsVectorFileWriter, QgsCoordinateReferenceSystem, \
+    QgsCoordinateTransformContext, QgsFeatureSink, QgsVectorLayer, QgsProject
+
+
+class SammoDataBase:
+    CONST_DB_NAME = "sammo-boat.gpkg"
+    CONST_LAYER_NAME = "session datas"
+
+    def __init__(self):
+        pass
+
+    def IsDataBaseAvailableInThisDirectory(self, directory):
+        return os.path.isfile(directory + "/" + self.CONST_DB_NAME)
+
+    def CreateEmptyDataBase(self, directory):
+        geom = QgsWkbTypes.Point
+        tableName = "emptyTable"
+        db = directory + "/" + self.CONST_DB_NAME
+
+        fields = QgsFields()
+
+        opts = QgsVectorFileWriter.SaveVectorOptions()
+        opts.driverName = "GPKG"
+        opts.layerName = tableName
+
+        if not os.path.isfile(db):
+            opts.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteFile
+        else:
+            opts.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteLayer
+
+        crs = QgsCoordinateReferenceSystem.fromEpsgId(4326)
+
+        QgsVectorFileWriter.create(
+                db,
+                fields,
+                geom,
+                crs,
+                QgsCoordinateTransformContext(),
+                opts,
+                QgsFeatureSink.SinkFlags(),
+                None,
+                tableName,
+                )
+
+    def LoadDataBase(self, directory):
+        db = directory + "/" + self.CONST_DB_NAME
+        vlayer = QgsVectorLayer(db, self.CONST_LAYER_NAME)
+        if not vlayer.isValid():
+            QMessageBox.critical(None, 'Sammo-Boat plugin', 'Impossible to read the file ' + self.CONST_DB_NAME)
+        else:
+            QgsProject.instance().addMapLayer(vlayer)
+
