c5cc06eabb28291e6d09e9044019b1d4961ff9b9
Fred1511 <secretariat@forsim.net>
2021-07-08 18:38:51 +0200

    All seems in place but no dialog shows up

--- sammo.py
+++ sammo.py
@@ -5,14 +5,24 @@ __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
 from .src.gui.session import SammoActionSession
 from .src.gui.onOffEffort import SammoActionOnOffEffort
+from .src.core.session import SammoSession
 
 
 class Sammo:
     def __init__(self, iface):
         self.iface = iface
         self.toolBar = self.iface.addToolBar("Sammo ToolBar")
-        self.actionSession = SammoActionSession(self.iface.mainWindow(), self.toolBar)
-        self.actionOnOffSession = SammoActionOnOffEffort(self.iface, self.toolBar)
+        self.session = SammoSession()
+        self.actionSession = SammoActionSession(
+            self.iface.mainWindow(),
+            self.toolBar,
+            self.session
+        )
+        self.actionOnOffSession = SammoActionOnOffEffort(
+            self.iface,
+            self.toolBar,
+            self.session
+        )
 
     def initGui(self):
         self.actionSession.initGui()
