851f4744d060c6f94430793e5b1ffe28b1264188
Fred1511 <secretariat@forsim.net>
2021-07-09 17:28:44 +0200

    the database code removed from onOffEffort class (GUI) to SammoSession class

--- src/core/session.py
+++ src/core/session.py
@@ -4,6 +4,8 @@ __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
 from .database import SammoDataBase
+from qgis.PyQt.QtWidgets import QMessageBox
+from qgis.core import QgsVectorLayerUtils
 
 
 class SammoSession:
@@ -21,3 +23,25 @@ class SammoSession:
 
     def loadTable(self, tableName):
         return self.db.loadTable(self.directoryPath, tableName)
+
+    def getReadyToAddNewFeatureToEnvironmentTable(self):
+        return self._getReadyToAddNewFeature(SammoDataBase.ENVIRONMENT_TABLE_NAME)
+
+    def addNewFeatureToEnvironmentTable(self, table, feat):
+        table.addFeature(feat)
+        table.commitChanges()
+
+    def _getReadyToAddNewFeature(self, tableName):
+        table = self.loadTable(tableName)
+        if not table.isValid():
+            QMessageBox.critical(
+                None,
+                "Sammo-Boat plugin",
+                "Impossible to read the table " + tableName
+            )
+            return
+
+        feat = QgsVectorLayerUtils.createFeature(table)
+        table.startEditing()
+
+        return [feat, table]
