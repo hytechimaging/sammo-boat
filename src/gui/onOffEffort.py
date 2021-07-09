2d150899dbf02287e2a79f7eca89845c1758fea1
Fred1511 <secretariat@forsim.net>
2021-07-09 17:53:55 +0200

    stop giving iface access to SammoActionOnOffEffort class

--- src/gui/onOffEffort.py
+++ src/gui/onOffEffort.py
@@ -3,26 +3,43 @@
 __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
+from abc import abstractmethod
 from qgis.PyQt.QtWidgets import QPushButton
 
-from ..core.database import SammoDataBase
+
+class ParentOfSammoActionOnOffEffort:
+    """
+    This is the abstract class that Sammo class
+    (which is the owner of the SammoActionOnOffEffort
+    instance) needs to inherit from
+    """
+
+    @property
+    @abstractmethod
+    def mainWindow(self):
+        pass
+
+    @property
+    @abstractmethod
+    def toolBar(self):
+        pass
+
+    @abstractmethod
+    def openDialogToAddNewFeatureToEnvironmentTable(self):
+        pass
 
 
 class SammoActionOnOffEffort:
-    def __init__(self, iface, toolBar, session):
-        self.iface = iface
-        self.mainWindow = self.iface.mainWindow()
-        self.action = None
-        self.session = session
-        self.toolBar = toolBar
+    def __init__(self, parent : ParentOfSammoActionOnOffEffort):
+        self.parent = parent
         self.button = None
 
     def initGui(self):
-        self.button = QPushButton(self.mainWindow)
+        self.button = QPushButton(self.parent.mainWindow)
         self.button.setText("Effort")
         self.button.clicked.connect(self.run)
         self.button.setEnabled(False)
-        self.toolBar.addWidget(self.button)
+        self.parent.toolBar.addWidget(self.button)
 
     def onCreateSession(self):
         self.button.setEnabled(True)
@@ -31,9 +48,4 @@ class SammoActionOnOffEffort:
         del self.button
 
     def run(self):
-        print("Effort button pressed")
-
-        [feat, table] = self.session.getReadyToAddNewFeatureToEnvironmentTable()
-        if self.iface.openFeatureForm(table, feat):
-            self.session.addNewFeatureToEnvironmentTable(table, feat)
-
+        self.parent.openDialogToAddNewFeatureToEnvironmentTable()
