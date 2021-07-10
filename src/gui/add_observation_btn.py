3c054a2929d620226c1c2010d24ca55f6794c617
fred1511 <secretariat@forsim.net>
2021-07-10 10:01:43 +0200

    T010_addingObservations - specifications test has succeeded

---
+++ src/gui/add_observation_btn.py
@@ -0,0 +1,55 @@
+# coding: utf8
+
+__contact__ = "info@hytech-imaging.fr"
+__copyright__ = "Copyright (c) 2021 Hytech Imaging"
+
+from abc import abstractmethod
+from qgis.PyQt.QtWidgets import QPushButton
+
+
+class IParentOfAddObservationBtn:
+    """
+    This is the abstract class that Sammo class
+    (which is the owner of the SammoActionAddObservation
+    instance) needs to inherit from
+    """
+
+    @property
+    @abstractmethod
+    def mainWindow(self):
+        pass
+
+    @property
+    @abstractmethod
+    def toolBar(self):
+        pass
+
+    @abstractmethod
+    def onClicObservation(self):
+        pass
+
+
+class AddObservationBtn:
+    def __init__(self, parent: IParentOfAddObservationBtn):
+        self.parent = parent
+        self.button = None
+
+    def initGui(self):
+        self.button = QPushButton(self.parent.mainWindow)
+        self.button.setText("Observation")
+        self.button.clicked.connect(self.run)
+        self.button.setEnabled(False)
+        self.button.setCheckable(True)
+        self.parent.toolBar.addWidget(self.button)
+
+    def onStartSession(self):
+        self.button.setEnabled(True)
+
+    def onStopEffort(self):
+        self.button.setEnabled(False)
+
+    def unload(self):
+        del self.button
+
+    def run(self):
+        self.parent.onClicObservation()
