dc134bbb62d7d259030e5565e9cfe9996a1c0f6f
fred1511 <secretariat@forsim.net>
2021-07-15 19:21:44 +0200

    T005 - sound recording succeeded with any duration

--- src/core/thread_sound_recording.py
+++ src/core/thread_sound_recording.py
@@ -6,29 +6,31 @@ __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 from .other_thread import WorkerForOtherThread, OtherThread
 import sounddevice as sd
 from scipy.io.wavfile import write
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
+import queue
+
+q = queue.Queue()
+
+
+def callback(indata, frames, time, status):
+    q.put(indata.copy())
 
 
 class WorkerForSoundRecording(WorkerForOtherThread):
     def __init__(self, soundFilePath: str):
         super().__init__()
         self._soundFilePath = soundFilePath
-        self.isStopRecordingClicked = False
+        self.fs = 44100
 
     def _toDoInsideLoop(self):
-        pass
-
-    def onRecordIsFinished(self):
-        write(self._soundFilePath, self.fs, self.myRecording)  # Save as WAV file
-
-    def _onStart(self):
-        self.fs = 44100  # Sample rate
-        seconds = 60  # Duration of recording
-        self.myRecording = sd.rec(int(seconds * self.fs), samplerate=self.fs, channels=2)
-
-    def stop(self):
-        sd.stop()
-        self.onRecordIsFinished()
-        super().stop()
+        # Make sure the file is opened before recording anything:
+        with sf.SoundFile(self._soundFilePath, mode='x', samplerate=self.fs,
+                      channels=2) as file:
+            with sd.InputStream(samplerate=self.fs, channels=2, callback=callback):
+                while not self._isNeedToStop:
+                    file.write(q.get())
 
 
 class ThreadForSoundRecording(OtherThread):
