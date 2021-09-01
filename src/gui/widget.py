425c52283c1a2ee6f58296a991382affa52eb305
fred1511 <secretariat@forsim.net>
2021-09-01 15:30:03 +0200

    t026 - widget with 5 buttons

--- src/gui/widget.py
+++ src/gui/widget.py
@@ -4,7 +4,7 @@ __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtWidgets import QListWidget, QDockWidget
+from qgis.PyQt.QtWidgets import QDockWidget, QWidget, QHBoxLayout, QPushButton
 
 
 class Widget:
@@ -24,15 +24,10 @@ class Widget:
 
     def createDockWidget(self):
         self.dock = QDockWidget(Widget.widgetName, self.iface.mainWindow())
-        customerList = QListWidget(self.dock)
-        items = ["John Doe, Harmony Enterprises, 12 Lakeside, Ambleton",
-                 "Jane Doe, Memorabilia, 23 Watersedge, Beaton",
-                 "Tammy Shea, Tiblanka, 38 Sea Views, Carlton",
-                 "Tim Sheen, Caraba Gifts, 48 Ocean Way, Deal",
-                 "Sol Harvey, Chicos Coffee, 53 New Springs, Eccleston",
-                 "Sally Hobart, Tiroli Tea, 67 Long River, Fedula"
-                 ]
-        customerList.addItems(items)
-        self.dock.setWidget(customerList)
         self.iface.addDockWidget(Qt.TopDockWidgetArea, self.dock)
-        self.dock.setWindowTitle(Widget.widgetName)
+
+        self.internalWidget = QWidget(self.dock)
+        self.dock.setWidget(self.internalWidget)
+        self.internalWidget.setLayout(QHBoxLayout())
+        for i in range(5):
+            self.internalWidget.layout().addWidget(QPushButton("{}".format(i)))
