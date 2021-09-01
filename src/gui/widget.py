d0f2dbe628009abc4290b7218877802c0e4101b0
fred1511 <secretariat@forsim.net>
2021-09-01 21:25:12 +0200

    t026 - widgets cosmetic

--- src/gui/widget.py
+++ src/gui/widget.py
@@ -4,7 +4,8 @@ __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtWidgets import QDockWidget, QWidget, QHBoxLayout, QPushButton
+from qgis.PyQt import QtGui
+from qgis.PyQt.QtWidgets import QDockWidget, QWidget, QGridLayout, QVBoxLayout, QLabel
 
 
 class Widget:
@@ -21,9 +22,50 @@ class Widget:
 
         self.internalWidget = QWidget(self.dock)
         self.dock.setWidget(self.internalWidget)
-        self.internalWidget.setLayout(QHBoxLayout())
-        for i in range(5):
-            self.internalWidget.layout().addWidget(QPushButton("{}".format(i)))
+        self.internalWidget.setLayout(QGridLayout())
+
+        gpsWidget = self.createGpsWidget()
+        effortLabel = self.createEffortLabel()
+        soundRecordingLabel = self.createSoundRecordingLabel()
+        self.internalWidget.layout().addWidget(gpsWidget, 0, 0)
+        self.internalWidget.layout().addWidget(effortLabel, 0, 1)
+        self.internalWidget.layout().addWidget(soundRecordingLabel, 0, 2)
+
+    def createEffortLabel(self) -> QLabel:
+        label = QLabel("ON/OFF effort")
+        label.setStyleSheet("QLabel { background-color : rgb(150,150,150); color : black; }")
+        label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        myFont= QtGui.QFont()
+        myFont.setBold(True)
+        label.setFont(myFont)
+
+        return label
+
+    def createSoundRecordingLabel(self) -> QLabel:
+        label = QLabel("Recording")
+        label.setStyleSheet("QLabel { background-color : rgb(200,255,200); color : black; }")
+        label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        myFont= QtGui.QFont()
+        myFont.setBold(True)
+        label.setFont(myFont)
+
+        return label
+
+    def createGpsWidget(self) -> QWidget:
+        gpsWidget = QWidget(self.dock)
+        gpsWidget.setLayout(QVBoxLayout())
+        gpsWidget.setStyleSheet("QWidget { background-color : rgb(255,150,150); color : black; }")
+        title = QLabel("GPS online")
+        title.setAlignment(Qt.AlignCenter)
+        myFont= QtGui.QFont()
+        myFont.setBold(True)
+        title.setFont(myFont)
+
+        gpsWidget.layout().addWidget(title)
+        gpsWidget.layout().addWidget(QLabel("Latitude : XXX"))
+        gpsWidget.layout().addWidget(QLabel("Longitude : YYY"))
+
+        return gpsWidget
 
     def _isDockWidgetExists(self) -> bool :
         for dockWidget in self.iface.mainWindow().findChildren(QDockWidget):
