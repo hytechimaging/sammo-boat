778e1c58409e4f789314e72a40d63476b610266b
fred1511 <secretariat@forsim.net>
2021-09-01 20:31:47 +0200

    t026 - cosmetic

--- src/gui/widget.py
+++ src/gui/widget.py
@@ -12,14 +12,7 @@ class Widget:
 
     def __init__(self, iface):
         self.iface = iface
-        dockWidgetFound = False
-        for dockWidget in self.iface.mainWindow().findChildren(QDockWidget):
-            print(dockWidget.windowTitle())
-            if dockWidget.windowTitle() == Widget.widgetName:
-                dockWidgetFound = True
-                print("FOUND !!!!!!!!!!!!!!!")
-                break
-        if not dockWidgetFound:
+        if not self._isDockWidgetExists():
             self.createDockWidget()
 
     def createDockWidget(self):
@@ -31,3 +24,9 @@ class Widget:
         self.internalWidget.setLayout(QHBoxLayout())
         for i in range(5):
             self.internalWidget.layout().addWidget(QPushButton("{}".format(i)))
+
+    def _isDockWidgetExists(self) -> bool :
+        for dockWidget in self.iface.mainWindow().findChildren(QDockWidget):
+            if dockWidget.windowTitle() == Widget.widgetName:
+                return True
+        return False
