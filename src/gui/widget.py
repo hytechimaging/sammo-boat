f68abd6353b80eeec6d449898d0d2acac5713226
fred1511 <secretariat@forsim.net>
2021-09-01 15:15:29 +0200

    t026 - widget created from scratches

--- src/gui/widget.py
+++ src/gui/widget.py
@@ -3,19 +3,36 @@
 __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
-import os
-from PyQt5.QtCore import QObject, Qt
-from qgis.PyQt import uic, QtGui, QtWidgets
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QListWidget, QDockWidget
 
 
-class Widget(QObject):
+class Widget:
+    widgetName = "MyDock"
 
-    def __init__ (self,iface):
-        QObject.__init__(self)
+    def __init__(self, iface):
         self.iface = iface
-        path = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(path, "widget.ui")
-        print(path)
-        self.dock = uic.loadUi(path)
+        dockWidgetFound = False
+        for dockWidget in self.iface.mainWindow().findChildren(QDockWidget):
+            print(dockWidget.windowTitle())
+            if dockWidget.windowTitle() == Widget.widgetName:
+                dockWidgetFound = True
+                print("FOUND !!!!!!!!!!!!!!!")
+                break
+        if not dockWidgetFound:
+            self.createDockWidget()
+
+    def createDockWidget(self):
+        self.dock = QDockWidget(Widget.widgetName, self.iface.mainWindow())
+        customerList = QListWidget(self.dock)
+        items = ["John Doe, Harmony Enterprises, 12 Lakeside, Ambleton",
+                 "Jane Doe, Memorabilia, 23 Watersedge, Beaton",
+                 "Tammy Shea, Tiblanka, 38 Sea Views, Carlton",
+                 "Tim Sheen, Caraba Gifts, 48 Ocean Way, Deal",
+                 "Sol Harvey, Chicos Coffee, 53 New Springs, Eccleston",
+                 "Sally Hobart, Tiroli Tea, 67 Long River, Fedula"
+                 ]
+        customerList.addItems(items)
+        self.dock.setWidget(customerList)
         self.iface.addDockWidget(Qt.TopDockWidgetArea, self.dock)
-        self.dock.setVisible(True)
+        self.dock.setWindowTitle(Widget.widgetName)
