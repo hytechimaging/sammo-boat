c216d70c6598c544301e9a987fa28052ea90ae34
fred1511 <secretariat@forsim.net>
2021-09-01 20:21:17 +0200

    t016 - start from empty project

--- src/core/session.py
+++ src/core/session.py
@@ -57,6 +57,7 @@ class SammoSession:
             + "?projectName=sammo_boat_project"
         )
         if isNewDataBase:
+            QgsProject.instance().clear()
             # Save the QGIS projet into the database
             QgsProject.instance().addMapLayer(self._gpsTable)
             QgsProject.instance().write(uri)
