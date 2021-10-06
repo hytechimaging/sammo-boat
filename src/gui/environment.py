dea08ed308895fd0c29a481d1487122f31a609e8
Blottiere Paul <blottiere.paul@gmail.com>
2021-10-06 21:19:14 +0200

    Add icon to environment button

--- src/gui/environment.py
+++ src/gui/environment.py
@@ -3,10 +3,11 @@
 __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
-from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtCore import pyqtSignal, QObject
 from qgis.core import QgsVectorLayer, QgsFeature
-from qgis.PyQt.QtWidgets import QPushButton, QToolBar
+from qgis.PyQt.QtWidgets import QAction, QToolBar
+
+from ..core import icon
 
 
 class SammoEnvironmentAction(QObject):
@@ -16,24 +17,25 @@ class SammoEnvironmentAction(QObject):
     def __init__(self, parent: QObject, toolbar: QToolBar):
         super().__init__()
         self.parent = parent
-        self.button: QIcon = None
+        self.action: QAction = None
         self.initGui(parent, toolbar)
 
     def initGui(self, parent: QObject, toolbar: QToolBar):
-        self.button = QPushButton(parent)
-        self.button.setText("Change environment")
-        self.button.clicked.connect(self.onClick)
-        self.button.setEnabled(False)
-        toolbar.addWidget(self.button)
+        self.action = QAction(parent)
+        self.action.setIcon(icon("environment.png"))
+        self.action.setToolTip("Update environment")
+        self.action.triggered.connect(self.onClick)
+        self.action.setEnabled(False)
+        toolbar.addAction(self.action)
 
     def onChangeEffortStatus(self, effortStatus: bool):
-        self.button.setEnabled(effortStatus)
+        self.action.setEnabled(effortStatus)
 
     def setEnabled(self, status: bool) -> None:
-        self.button.setEnabled(status)
+        self.action.setEnabled(status)
 
     def unload(self):
-        del self.button
+        del self.action
 
     def onClick(self):
         self.triggered.emit()
