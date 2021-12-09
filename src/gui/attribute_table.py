75d6ad4e5ff6e86479663104e4a9c35942a2b2f5
Blottiere Paul <blottiere.paul@gmail.com>
2021-12-09 17:13:22 +0100

    Open follower table on click

---
+++ src/gui/attribute_table.py
@@ -0,0 +1,62 @@
+# coding: utf8
+
+__contact__ = "info@hytech-imaging.fr"
+__copyright__ = "Copyright (c) 2021 Hytech Imaging"
+
+import os
+import sys
+
+from qgis.PyQt import uic
+from qgis.PyQt.QtCore import Qt, QSize
+from qgis.PyQt.QtWidgets import QFrame, QLabel, QDockWidget, QTableView, QAction, QHeaderView, QToolBar
+
+from qgis.core import QgsSettings
+
+from ..core import pixmap
+from ..core.thread_widget import ThreadWidget
+
+
+class SammoAttributeTable():
+
+    @staticmethod
+    def toolbar(table):
+        return table.findChild(QToolBar, "mToolbar")
+
+    @staticmethod
+    def refresh(table):
+        table.findChild(QAction, "mActionReload").trigger()
+        table.findChild(QTableView).resizeColumnsToContents()
+
+    @staticmethod
+    def attributeTable(iface, layer):
+        # hide some columns
+        hiddens = ["fid", "soundFile", "soundStart", "soundEnd"]
+        config = layer.attributeTableConfig()
+        columns = config.columns()
+        for column in columns:
+            if column.name in hiddens:
+                column.hidden = True
+        config.setColumns( columns )
+        layer.setAttributeTableConfig( config )
+
+        # init attribute table
+        table = iface.showAttributeTable(layer)
+
+        # hide some items
+        last = table.layout().rowCount() - 1
+        layout = table.layout().itemAtPosition(last, 0).itemAt(0)
+        for idx in range(layout.count()):
+            layout.itemAt(idx).widget().hide()
+
+        layout = table.findChild(QFrame, "mUpdateExpressionBox").layout()
+        for idx in range(layout.count()):
+            layout.itemAt(idx).widget().hide()
+
+        SammoAttributeTable.toolbar(table).hide()
+
+        # update table view
+        view = table.findChild(QTableView)
+        view.horizontalHeader().setStretchLastSection(True)
+        view.sortByColumn(0, Qt.DescendingOrder)
+
+        return table
--- src/gui/follower.py
+++ src/gui/follower.py
@@ -3,10 +3,18 @@
 __contact__ = "info@hytech-imaging.fr"
 __copyright__ = "Copyright (c) 2021 Hytech Imaging"
 
+import os
+
+from qgis.PyQt import uic
 from qgis.PyQt.QtCore import pyqtSignal, QObject
-from qgis.PyQt.QtWidgets import QAction, QToolBar
+from qgis.PyQt.QtWidgets import QAction, QToolBar, QFrame, QDialog
 
 from ..core import icon
+from .attribute_table import SammoAttributeTable
+
+FORM_CLASS, _ = uic.loadUiType(
+    os.path.join(os.path.dirname(__file__), "ui/follower.ui")
+)
 
 
 class SammoFollowerAction(QObject):
@@ -33,3 +41,16 @@ class SammoFollowerAction(QObject):
 
     def onClick(self):
         self.triggered.emit()
+
+
+class SammoFollowerTable(QDialog, FORM_CLASS):
+
+    def __init__(self, iface, followerLayer):
+        super().__init__()
+        self.iface = iface
+        self.setupUi(self)
+        self.table = SammoAttributeTable.attributeTable(iface, followerLayer)
+        self.verticalLayout.addWidget(self.table)
+
+    def refresh(self):
+        SammoAttributeTable.refresh(self.table)
