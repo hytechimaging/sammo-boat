60a11ac613c9ec48680a0b71969f74c29fcb35ee
Blottiere Paul <blottiere.paul@gmail.com>
2021-12-14 09:43:55 +0100

    Wire buttons and lineedits

--- src/gui/merge.py
+++ src/gui/merge.py
@@ -7,8 +7,8 @@ import os
 import pygeodiff
 
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import pyqtSignal, QObject
-from qgis.PyQt.QtWidgets import QAction, QToolBar, QDialog
+from qgis.PyQt.QtCore import pyqtSignal, QObject, QDir
+from qgis.PyQt.QtWidgets import QAction, QToolBar, QDialog, QFileDialog
 
 from ..core import utils
 from .attribute_table import SammoAttributeTable
@@ -50,6 +50,42 @@ class SammoMergeDialog(QDialog, FORM_CLASS):
 
         self.ok.clicked.connect(self.merge)
         self.cancel.clicked.connect(self.close)
+        self.sessionAButton.clicked.connect(self.sessionA)
+        self.sessionBButton.clicked.connect(self.sessionB)
+        self.sessionMergedButton.clicked.connect(self.sessionMerged)
+
+    def sessionA(self):
+        sessionA = QFileDialog.getExistingDirectory(
+            None,
+            "Session 1",
+            QDir.currentPath(),
+            QFileDialog.DontUseNativeDialog,
+        )
+
+        if sessionA:
+            self.sessionADir.setText(sessionA)
+
+    def sessionB(self):
+        sessionB = QFileDialog.getExistingDirectory(
+            None,
+            "Session B",
+            QDir.currentPath(),
+            QFileDialog.DontUseNativeDialog,
+        )
+
+        if sessionB:
+            self.sessionBDir.setText(sessionB)
+
+    def sessionMerged(self):
+        sessionMerged = QFileDialog.getExistingDirectory(
+            None,
+            "Session merged",
+            QDir.currentPath(),
+            QFileDialog.DontUseNativeDialog,
+        )
+
+        if sessionMerged:
+            self.sessionMergedDir.setText(sessionMerged)
 
     def merge(self):
         pass
