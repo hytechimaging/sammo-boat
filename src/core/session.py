a47d3a57607e010a99b19d5196edd73af89cb0ad
SebastienPeillet <peillet.seb@gmail.com>
2022-03-02 17:34:11 +0100

    add administrator table and update environment record with validation action

--- src/core/session.py
+++ src/core/session.py
@@ -307,7 +307,6 @@ class SammoSession:
             + self.sightingsLayer.selectedFeatureCount()
             + self.followersLayer.selectedFeatureCount()
         )
-
         survey = (
             next(self.surveyLayer.getFeatures())
             if self.surveyLayer.featureCount()
