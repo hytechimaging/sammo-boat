7209036de18406442e1ef14a9f093b49051e0044
SebastienPeillet <peillet.seb@gmail.com>
2022-10-31 14:58:52 +0100

    no duplicate of comment (#6)

--- src/core/session.py
+++ src/core/session.py
@@ -570,6 +570,7 @@ class SammoSession:
                         "transect",
                         "strate",
                         "length",
+                        "comment"
                     ]:
                         continue
                     else:
